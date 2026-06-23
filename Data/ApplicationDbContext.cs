ef94a88466f1370095cb982a31266820ea1bf2b2
Omargenao25 <omargenao78@gmail.com>
2026-06-23 17:59:45 -0400

    creacion de la carpeta Data para el contexto de la base de datos

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Actividad3ApiDB.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Actividad3ApiDB.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Libro> Libros { get; set; }
+    }
+}
