d0a5363fbbc00c2fe396a574229f543e8f2165d8
Omargenao25 <omargenao78@gmail.com>
2026-06-23 21:52:02 -0400

    creacion y avances en libro controller

---
+++ Controllers/LibrosController.cs
@@ -0,0 +1,19 @@
+﻿using Actividad3ApiDB.Data;
+using Actividad3ApiDB.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Actividad3ApiDB.Controllers
+{
+    [Route("api/libros")]
+    [ApiController]
+    public class LibrosController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LibrosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
