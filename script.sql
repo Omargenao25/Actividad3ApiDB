03e76fea2a4b52194420c5833223f23f3063d723
Omargenao25 <omargenao78@gmail.com>
2026-06-23 21:29:19 -0400

    creacion script de la base de datos

---
+++ script.sql
@@ -0,0 +1,33 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+CREATE TABLE [Libros] (
+    [Id] int NOT NULL IDENTITY,
+    [Titulo] nvarchar(max) NOT NULL,
+    [Autor] nvarchar(max) NOT NULL,
+    [AnioPublicacion] int NOT NULL,
+    [Genero] nvarchar(max) NOT NULL,
+    [NumeroPaginas] int NOT NULL,
+    [Precio] decimal(18,2) NOT NULL,
+    [Disponible] bit NOT NULL,
+    CONSTRAINT [PK_Libros] PRIMARY KEY ([Id])
+);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20260624003422_DBbiblioteca', N'8.0.11');
+GO
+
+COMMIT;
+GO
+
